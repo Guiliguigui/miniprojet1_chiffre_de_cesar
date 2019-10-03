2f724e499904fc42b32f97520d4e69671991b3a6
Andrp99 <andreas.pierre@etud.u-picardie.fr>
2019-10-03 09:36:40 +0200

    fin de l implementation des tests et fonctions

--- chiffre_de_cesar.py
+++ chiffre_de_cesar.py
@@ -1,11 +1,24 @@
-def chiffrage(caractere):
-    valeur_ascii=ord(caractere)
-    valeur_ascii=valeur_ascii+8
-    caractere_chiffre=chr(valeur_ascii)
-    return caractere_chiffre
+def chiffrage(liste):
+    chaine_chiffre=""
+    for caractere in liste:
+        valeur_ascii=ord(caractere)
+        valeur_ascii=valeur_ascii+8
+        caractere_chiffre=chr(valeur_ascii)
+        chaine_chiffre=''.join([chaine_chiffre,caractere_chiffre,])
+    return chaine_chiffre
 
-def dechiffrage(caractere):
-    valeur_ascii=ord(caractere)
-    valeur_ascii=valeur_ascii-8
-    caractere_dechiffre=chr(valeur_ascii)
-    return caractere_dechiffre
+def dechiffrage(liste):
+    chaine_dechiffre=""
+    for caractere in liste:
+        valeur_ascii=ord(caractere)
+        valeur_ascii=valeur_ascii-8
+        caractere_dechiffre=chr(valeur_ascii)
+        chaine_dechiffre=''.join([chaine_dechiffre,caractere_dechiffre,])
+    return chaine_dechiffre
+
+def decoupage(chaine):
+    liste = []
+    chaine=str(chaine)
+    for caractere in chaine:
+        liste.append(caractere)
+    return liste
--- tests/test_chiffre_de_cesar.py
+++ tests/test_chiffre_de_cesar.py
@@ -1,19 +1,21 @@
 import unittest
 from  chiffre_de_cesar import chiffrage
 from  chiffre_de_cesar import dechiffrage
+from  chiffre_de_cesar import decoupage
 
 class TestChiffreDeCesarFunctions(unittest.TestCase):
 
     def test_chiffrage(self):
-        self.assertEqual(chiffrage('$'), ',')
-        self.assertEqual(chiffrage('?'), 'G')
-        self.assertEqual(chiffrage('5'), '=')
-        self.assertEqual(chiffrage('W'), '_')
+        self.assertEqual(chiffrage(['t','e','s','t',]), "|m{|")
+        self.assertEqual(chiffrage(['l',']','k','l',]), "test")
 
     def test_dechiffrage(self):
-        self.assertEqual(dechiffrage('~'), 'v')
-        self.assertEqual(dechiffrage('-'), '%')
-        self.assertEqual(dechiffrage('<'), '4')
+        self.assertEqual(dechiffrage(['t','e','s','t',]), "l]kl")
+        self.assertEqual(dechiffrage(['|','m','{','|',]), "test")
+
+    def test_decoupage(self):
+        self.assertEqual(decoupage("test"),['t','e','s','t',])
+        self.assertEqual(decoupage(82),['8','2',])
         
 
 
