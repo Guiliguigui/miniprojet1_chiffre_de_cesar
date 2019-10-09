ce758338648bcc212b1fe708060d3b85091195cc
Guillaume <gui.mairesse@gmail.com>
2019-10-09 18:42:36 +0200

    zone d'affichage

--- form_chiffre_de_cesar.py
+++ form_chiffre_de_cesar.py
@@ -3,12 +3,13 @@ from chiffre_de_cesar import *
 
 def button_chiffrer_callback():
     liste=decoupage(value.get())
-    label.
-     chiffrage(liste)
+    textarea.delete("0.0",END)
+    textarea.insert(END, chiffrage(liste))
 
 def button_dechiffrer_callback():
     liste=decoupage(value.get())
-    dechiffrage(liste)
+    textarea.delete("0.0",END)
+    textarea.insert(END, dechiffrage(liste))
 
 frame = Tk()
 frame.title("Chiffrement de Cesar")
@@ -16,7 +17,7 @@ frame.title("Chiffrement de Cesar")
 value = StringVar() 
 value.set("texte par défaut")
 entry = Entry(frame, textvariable=value, width=30)
-entry.grid(column=0, row=0) 
+entry.grid(column=0, row=0,columnspan=2) 
 
 button_chiffrer = Button(frame, text="Chiffrer", command=button_chiffrer_callback)
 button_chiffrer.grid(column=0, row=1)
@@ -24,7 +25,8 @@ button_chiffrer.grid(column=0, row=1)
 button_dechiffrer = Button(frame, text="Dechiffrer", command=button_dechiffrer_callback)
 button_dechiffrer.grid(column=1, row=1)
 
-label=Label(root, text="")
-label.grid(column=0, row=2)
+textarea = Text(frame, wrap='word')
+textarea.grid(column=0,row=2,columnspan=2)
+textarea.insert(END, "Hello World")
 
 frame.mainloop()
